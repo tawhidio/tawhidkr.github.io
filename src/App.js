ce0b07aa663a79de8c941bc309d44464e9291e48
sidcode <sidcode@gmail.com>
2020-12-13 00:13:46 +0900

    메인 페이지 수정 및 package.json 등등 수정

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          나의 머신러닝과 딥러닝 공간<br/>
+          My Machine Learning and Deep Learning Space 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://https://tawhidkr.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Speace
         </a>
       </header>
     </div>
